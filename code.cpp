5f67554d96aa625d8832c31d36e078a55297d401
Jashanpreet Singh <77024639+ogjash@users.noreply.github.com>
2025-01-27 23:37:15 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,19 +1,15 @@
 #pragma GCC optimize("O3,unroll-loops")
 
 #include<bits/stdc++.h>
-
 using namespace std;
-using namespace chrono;
 
 #define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
 #define nl "\n"
 #define yes cout<<"YES"<<nl
 #define no cout<<"NO"<<nl
+#define ll long long
+#define ull unsigned long long
 #define vll vector<ll>
-#define vi vector<int>
 #define vvi vector<vector<int>> 
 #define vii vector<pair<int, int>> 
 #define pii pair<int, int>
@@ -23,10 +19,6 @@ using namespace chrono;
 #else
 #define debug(x);
 #endif
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
 
 void solve() {
     int t;
