4c6e23cbeaa8f3596f36c517664bb875f95ac31c
Jashanpreet Singh <77024639+ogjash@users.noreply.github.com>
2025-01-29 23:31:08 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,11 +1,10 @@
-#pragma GCC optimize("O3,unroll-loops")
-
 #include<bits/stdc++.h>
 using namespace std;
 using namespace chrono;
 
 #define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define nl "\n"
+#define sp " "
 #define yes cout<<"YES"<<nl
 #define no cout<<"NO"<<nl
 #define ll long long
@@ -15,29 +14,16 @@ using namespace chrono;
 #define vii vector<pair<int, int>> 
 #define pii pair<int, int>
 
-#ifdef OG
-#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
-#else
-#define debug(x);
-#endif
+void OG() {
+        
+}
 
-void solve() {
+int main() {
+    fastio();
     int t;
     cin>>t;
     while(t--){
-        
+        OG();
     }
-}
-int main() {
-    fastio();
-#ifdef OG
-    freopen("Error.txt", "w", stderr);
-#endif
-        auto start1 = high_resolution_clock::now();
-        solve();
-        auto stop1 = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(stop1 - start1);
-#ifdef OG
-        cerr << "Time: " << duration . count() / 1000 << endl;
-#endif
+    return 0;
 }
